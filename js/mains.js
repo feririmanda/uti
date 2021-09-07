cf5d71e1b20fa6d771a057ed4b15a1759f235a22
Feri Rimanda <feririmanda@gmail.com>
2021-09-07 17:24:49 +0700

    header dan our service ok

--- js/mains.js
+++ js/mains.js
@@ -1,161 +1,135 @@
-$(window).on("load", function(){
-    // -------------- preloader ---------------------
-    $(".preloader").fadeOut("slow");
+$(window).on("load", function () {
+  // -------------- preloader ---------------------
+  $(".preloader").fadeOut("slow");
 });
 
-$(document).ready(function(){
-
-    // ------------------ Navbar Shrink ---------------
-    $(window).on("scroll", function(){
-        if($(this).scrollTop() > 90){
-            $(".navbar").addClass("navbar-shrink");
-        }
-        else{
-            $(".navbar").removeClass("navbar-shrink");
-        }
-    });
-    // ------------------ Video Popup -----------------
-    const videoSrc = $("#player-1").attr("src");
-    $(".video-play-btn, .video-popup").on("click", function(){
-       if($(".video-popup").hasClass("open")){
-           $(".video-popup").removeClass("open");
-           $("#player-1").attr("src", "");
-       }
-       else{
-           $(".video-popup").addClass("open");
-           if($("#player-1").attr("src") == ''){
-               $("#player-1").attr("src", videoSrc);
-           }
-       }
-    });
-
-    // --------------- Features Carousel -----------------
-    $('.features-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:3,
-            }
-        }
-    });
-
-        // --------------- App Screenshots Carousel -----------------
-    $('.screenshots-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:4,
-            }
-        }
-    });
-
-        // --------------- Testimonials Carousel -----------------
-    $('.testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:3,
-            }
-        }
-    });
-
-        // --------------- Team Carousel -----------------
-    $('.team-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:3,
-            }
-        }
-    });
-
-    // --------------- Page Scrolling - ScrollIt -----------------
-        $.scrollIt({
-            topOffset: -50
-        });
-        
-    // ---------------- Navbar Collapse ------------------------
-    $(".nav-link").on("click", function(){
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // ------------------- Toggle Theme - Light and Dark Mode --------------------
-    function toggleTheme(){
-        if(localStorage.getItem("feri-theme") !== null){
-            if(localStorage.getItem("feri-theme") === "dark"){
-                $("body").addClass("dark");
-            }
-            else{
-                 $("body").removeClass("dark");
-            }
-        }
-        updateIcon();
+$(document).ready(function () {
+  // ------------------ Navbar Shrink ---------------
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 90) {
+      $(".navbar").addClass("navbar-shrink");
+    } else {
+      $(".navbar").removeClass("navbar-shrink");
     }
+  });
 
-    toggleTheme();
+  // --------------- Features Carousel -----------------
+  $(".features-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 
-    $(".toggle-theme").on("click", function(){
-        $("body").toggleClass("dark");
-        if($("body").hasClass("dark")){
-            localStorage.setItem("feri-theme", "dark");
-        }else{
-            localStorage.setItem("feri-theme", "light");
-        }
-        updateIcon();
-    });
+  // --------------- App Screenshots Carousel -----------------
+  $(".screenshots-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
 
-    function updateIcon(){
-        if($("body").hasClass("dark")){
-            $(".toggle-theme i").removeClass("fa-moon");
-            $(".toggle-theme i").addClass("fa-sun");
-        }
-        else{
-             $(".toggle-theme i").removeClass("fa-sun");
-            $(".toggle-theme i").addClass("fa-moon");
-        }
+  // --------------- Testimonials Carousel -----------------
+  $(".testimonials-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  // --------------- Team Carousel -----------------
+  $(".team-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  // --------------- Page Scrolling - ScrollIt -----------------
+  $.scrollIt({
+    topOffset: -50,
+  });
+
+  // ---------------- Navbar Collapse ------------------------
+  $(".nav-link").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // ------------------- Toggle Theme - Light and Dark Mode --------------------
+  function toggleTheme() {
+    if (localStorage.getItem("feri-theme") !== null) {
+      if (localStorage.getItem("feri-theme") === "dark") {
+        $("body").addClass("dark");
+      } else {
+        $("body").removeClass("dark");
+      }
     }
+    updateIcon();
+  }
+
+  toggleTheme();
+
+  $(".toggle-theme").on("click", function () {
+    $("body").toggleClass("dark");
+    if ($("body").hasClass("dark")) {
+      localStorage.setItem("feri-theme", "dark");
+    } else {
+      localStorage.setItem("feri-theme", "light");
+    }
+    updateIcon();
+  });
+
+  function updateIcon() {
+    if ($("body").hasClass("dark")) {
+      $(".toggle-theme i").removeClass("fa-moon");
+      $(".toggle-theme i").addClass("fa-sun");
+    } else {
+      $(".toggle-theme i").removeClass("fa-sun");
+      $(".toggle-theme i").addClass("fa-moon");
+    }
+  }
 });
